c0d253e06a80fde336cb51fe184c3a584ac34192
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-02-04 17:57:03 +0800

    Setup lazy routing for auth pages in home page

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -5,8 +5,34 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomePage
+    path: 'home',
+    component: HomePage,
+    children: [
+      {
+        path: 'feed',
+        loadChildren: () => import('../pages/feed/feed.module').then(
+          m => m.FeedPageModule
+        )
+      },
+      {
+        path: 'messages',
+        loadChildren: () => import('../pages/messages/messages.module').then(
+          m => m.MessagesPageModule
+        )
+      },
+      {
+        path: 'notifications',
+        loadChildren: () => import('../pages/notifications/notifications.module').then(
+          m => m.NotificationsPageModule
+        )
+      },
+      {
+        path: 'settings',
+        loadChildren: () => import('../pages/settings/settings.module').then(
+          m => m.SettingsPageModule
+        )
+      }
+    ]
   }
 ];
 
