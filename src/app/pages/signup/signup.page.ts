c2e58bab0663842cca274b101f7f6c656d62897c
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-02-05 14:36:04 +0800

    Setup signup auth function for signup page

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { StorageService } from 'src/app/services/storage.service';
+import { ToastService } from 'src/app/services/toast.service';
+import { ThrowStmt } from '@angular/compiler';
+import { AuthConstants } from 'src/app/config/auth-constants';
 
 @Component({
   selector: 'app-signup',
@@ -7,14 +12,57 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  postData = {
+    username: '',
+    email: '',
+    password: ''
+  };
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private authService: AuthService,
+    private storageService: StorageService,
+    private toastService: ToastService) { }
 
   ngOnInit() {
   }
 
+  validateInput() {
+    let username = this.postData.username.trim();
+    let password = this.postData.password.trim();
+    let email = this.postData.email.trim();
+
+    return (
+      this.postData.username && this.postData.password && this.postData.email && 
+      username.length > 0 && password.length > 0 && email.length > 0
+      );
+  }
+
   signup() {
-    this.router.navigate(['/login']);
+    if (this.validateInput()) {
+      this.authService.signup(this.postData).subscribe(
+        (res: any) => {
+          if (res.userData) {
+            //Store usre data
+            this.storageService
+            .store(AuthConstants.AUTH, res.userData)
+            .then(res => {
+              this.router.navigate(['home/feed']);
+            });
+          } else {
+            this.toastService.presentToast(
+              'Data already exists, please enter new details'
+            );
+          }
+        }, 
+        (error: any) => {
+          this.toastService.presentToast('Network Issues.');
+        }
+      );
+    } else {
+      this.toastService.presentToast (
+        'Please enter email, username or password.'
+      )
+    }
   }
 
 }
