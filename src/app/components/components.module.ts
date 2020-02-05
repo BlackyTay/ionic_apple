ea51c00580e4591c08eabb7d3cc62d41123c8c8f
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-02-05 10:02:24 +0800

    Declare and export child modules. Then import necessary module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SlidesComponent } from './slides/slides.component';
+import { StartButtonComponent } from './start-button/start-button.component';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SlidesComponent, StartButtonComponent
+  ],
+  exports: [
+    SlidesComponent, StartButtonComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule, FormsModule, IonicModule
   ]
 })
 export class ComponentsModule { }
