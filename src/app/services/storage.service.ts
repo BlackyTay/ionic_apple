46df0ee544ad02a14c9eae4e6999402e8093eaac
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-02-05 13:49:46 +0800

    Setup storage service

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+const { Storage } = Plugins;
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,31 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  // Store value
+  async store(storeKey: string, value: any) {
+    const encryptedValue = btoa(escape(JSON.stringify(value)));
+    await Storage.set({
+      key: storeKey,
+      value: encryptedValue
+    });
+  }
+
+  //Get value
+  async get(storageKey: string) {
+    const ret = await Storage.get({
+      key: storageKey
+    });
+    return JSON.parse(unescape(atob(ret.value)));
+  }
+
+  async removeStorageItem(storageKey: string) {
+    await Storage.remove({
+      key: storageKey
+    });
+  }
+
+  async clear() {
+    await Storage.clear();
+  }
 }
