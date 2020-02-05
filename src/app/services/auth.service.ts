4b8c6d11b54832317b49613151c3253ed62a819a
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-02-05 14:07:37 +0800

    Setup auth service for login signup and logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { StorageService } from './storage.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { pseudoRandomBytes } from 'crypto';
+import { AuthConstants } from '../config/auth-constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService,
+    private storageService: StorageService,
+    private router: Router) { }
+
+    login(postData: any): Observable<any> {
+      return this.httpService.post('login', postData);
+    }
+
+    signup(postData: any): Observable<any> {
+      return this.httpService.post('signup', postData);
+    }
+
+    logout() {
+      this.storageService.removeStorageItem(AuthConstants.AUTH).then(res => {
+        this.router.navigate(['/login']);
+      });
+    }
 }
