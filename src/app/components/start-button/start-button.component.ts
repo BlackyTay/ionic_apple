2d6fa6eff3a1a8a111cd0a055b96f3e1b01b1236
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-02-05 09:59:59 +0800

    Import Router and add navigateToLogin() in ts

--- src/app/components/start-button/start-button.component.ts
+++ src/app/components/start-button/start-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-button',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartButtonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {}
 
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
